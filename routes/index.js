54eac2d7763a92233b5ab14c060de1d717a52ff3
Quan Vu <quan.vutien@powergatesoftware.com>
2022-03-28 01:03:07 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+console.log("__dirname", __dirname);
+
+router.get('/download', function(req, res, next) {
+  const file = `${__dirname}/image.jpeg`;
+  res.download(file); // Set disposition and send it.
+});
 
 module.exports = router;
